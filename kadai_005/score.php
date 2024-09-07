c144e38c0a46ad514b447669a143685db74bee3f
yujihorikawa <lucky.boy.ikki@gmail.com>
2024-09-07 20:45:56 +0900

    Add files via upload

---
+++ kadai_005/score.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+
+<body>
+    
+    <p>
+        <?php
+        echo (80+60+55+40+100+25+80+95+30+60)/10;
+        ?>
+    </p>
+</body>
+
+</html>
