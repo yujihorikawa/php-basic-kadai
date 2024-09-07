85e00a4dfc878026b036fcca5c118bc149d12433
yujihorikawa <lucky.boy.ikki@gmail.com>
2024-09-08 07:10:23 +0900

    Add files via upload

--- kadai_005/score.php
+++ kadai_005/score.php
@@ -1,18 +1,34 @@
 <!DOCTYPE html>
 <html lang="ja">
-
 <head>
     <meta charset="UTF-8">
-    <title>PHP基礎編</title>
+    <title>PHP練習編</title>
 </head>
-
 <body>
-    
-    <p>
-        <?php
-        echo (80+60+55+40+100+25+80+95+30+60)/10;
-        ?>
-    </p>
-</body>
+<p>
 
+<?php
+// 数値を変数に代入する//
+$score1 = 80;
+$score2 = 50;
+$score3 = 55;
+$score4 = 40;
+$score5 = 100;
+$score6 = 25;
+$score7 = 80;
+$score8 = 95;
+$score9 = 30;
+$score10 = 60;
+
+// 10人の合計点
+$total_score = $score1 + $score2 + $score3 + $score4 + $score5 + $score6 + $score7 + $score8 + $score9 + $score10;
+
+// 10人の平均点
+$score_average = $total_score / 10;
+echo $score_average;
+
+?>
+
+</p>
+</body>
 </html>
