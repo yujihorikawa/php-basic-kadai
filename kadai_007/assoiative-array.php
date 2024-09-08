51d5254b3b73056b345323806249194c5e23e1d9
yujihorikawa <lucky.boy.ikki@gmail.com>
2024-09-08 18:44:34 +0900

    Add files via upload

---
+++ kadai_007/assoiative-array.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  
+  <title>Document</title>
+</head>
+<body>
+  <p>
+<?php
+$array=[
+  'name'=>'onion',
+  'price'=>'200',
+  'weight'=>'160',
+];
+print_r($array);
+?>
+
+  </p>
+</body>
+</html>
